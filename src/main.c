c6d70f5f3e364ac90c71074a538404451e5b43ad
Jeremy <cream@sourcre.am>
2020-02-07 22:30:43 -0800

    look through lexer

--- src/main.c
+++ src/main.c
@@ -5,11 +5,6 @@
 
 int main(int argc, char **argv)
 {
-	clock_t start, end;
-	double cpu_time_used;
-	start = clock();
 	famuz_parse(argv[1]);
-	end = clock();
-	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 	return 0;
 }
